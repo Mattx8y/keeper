b72449f78c09dacf1b0bedea80a658322fb63d00
iWillBanU <wrscr.surpertop@gmail.com>
2020-09-22 08:49:02 -0400

    Update SaveMessageListener.kt

--- src/main/kotlin/me/ddivad/keeper/listeners/SaveMessageListener.kt
+++ src/main/kotlin/me/ddivad/keeper/listeners/SaveMessageListener.kt
@@ -5,6 +5,7 @@ import me.ddivad.keeper.dataclasses.Configuration
 import me.ddivad.keeper.utilities.buildSavedMessageEmbed
 import me.jakejmattson.discordkt.api.extensions.jda.sendPrivateMessage
 import net.dv8tion.jda.api.events.message.guild.react.GuildMessageReactionAddEvent
+import net.dv8tion.jda.api.events.message.guild.react.GuildMessageReactionRemoveEvent
 
 class ModerationListener(private val configuration: Configuration) {
     @Subscribe
@@ -15,6 +16,17 @@ class ModerationListener(private val configuration: Configuration) {
         if (event.reaction.reactionEmote.name == configuration[event.guild.idLong]?.bookmarkReaction) {
             event.channel.retrieveMessageById(event.messageId).queue {
                 event.user.sendPrivateMessage(buildSavedMessageEmbed(it.author, it, event.channel, event.guild))
+            }
+        }
+    }
+    @Subscribe
+    fun onGuildMessageReactionRemoveEvent(event: GuildMessageReactionRemoveEvent) {
+        if (event.user.isBot) return
+        if (!configuration[event.guild.idLong]?.enabled!!) return
+        
+        if (event.reaction.reactionEmote.name == configuration[event.guild.idLong?].bookmarkReaction) {
+            event.user.openPrivateChannel().queue {
+                // somehow find the message that matches the id of the reaction message
             }
         }
     }
